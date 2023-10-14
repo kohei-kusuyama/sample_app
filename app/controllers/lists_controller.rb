e06eafe0615130e12618bad37a7f1a013f94d1e4
KoheiKusuyama <144124225+kohei-kusuyama@users.noreply.github.com>
2023-10-14 05:13:10 +0000

    [save 3-6]

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,42 @@
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    #1.2 データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメゾット実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    # redirect_to '/top'
+    redirect_to list_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+end
